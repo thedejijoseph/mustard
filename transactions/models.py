e17b84656a9f3e97d1059771563e18084c8f0bed
Deji <thedejijoseph@gmail.com>
2024-11-12 20:13:27 +0100

    rename reated_at to created_at

--- transactions/models.py
+++ transactions/models.py
@@ -6,7 +6,7 @@ import uuid
 class Transaction(models.Model):
     uid = models.AutoField(primary_key=True)
     transaction_id = models.UUIDField(default=uuid.uuid4, unique=True)
-    reated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     masked_pan = models.CharField(max_length=18)
     encrypted_pan = models.CharField()
