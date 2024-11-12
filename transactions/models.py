6e4e97d5a046752b3986e59af0fcf2ae90784196
Deji <thedejijoseph@gmail.com>
2024-11-12 20:19:35 +0100

    update transactions model

--- transactions/models.py
+++ transactions/models.py
@@ -6,9 +6,9 @@ import uuid
 class Transaction(models.Model):
     uid = models.AutoField(primary_key=True)
     transaction_id = models.UUIDField(default=uuid.uuid4, unique=True)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     masked_pan = models.CharField(max_length=18)
-    encrypted_pan = models.CharField()
-    encrypted_expiry = models.CharField()
-    amount = models.FloatField()
+    encrypted_pan = models.TextField()
+    encrypted_expiry = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
