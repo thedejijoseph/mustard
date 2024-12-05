3b7c0e43b4ffe499ce7df9155d91eb23bb47b60f
Deji <thedejijoseph@gmail.com>
2024-12-05 21:07:27 +0100

    add dockerfile

---
+++ config/gunicorn/staging.py
@@ -0,0 +1,28 @@
+"""Gunicorn *dev* config file"""
+
+# Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
+wsgi_app = "mustard.wsgi:application"
+
+# The granularity of Error log outputs
+loglevel = "debug"
+
+# The number of worker processes for handling requests
+workers = 2
+
+# The socket to bind
+bind = "0.0.0.0:8001"
+
+# Restart workers when code changes (development only!)
+reload = True
+
+# Write access and error info to /var/log
+accesslog = errorlog = "/var/log/gunicorn/staging.log"
+
+# Redirect stdout/stderr to log file
+capture_output = True
+
+# PID file so you can easily fetch process ID
+pidfile = "/var/run/gunicorn/staging.pid"
+
+# Daemonize the Gunicorn process (detach & enter background)
+daemon = True
